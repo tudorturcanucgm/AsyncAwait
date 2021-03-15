4b75ba3cfe525788af49ac5149af37e6179c7fe8
Tudor Turcanu <tudor.turcanu@cgm.com>
2021-03-15 10:25:13 +0200

    Initial Commit

---
+++ AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitApp.swift
+//  AsyncAwait
+//
+//  Created by Tudor Turcanu on 15.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
