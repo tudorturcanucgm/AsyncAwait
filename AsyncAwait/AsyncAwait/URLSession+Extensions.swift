c491e4152d8f2a4afc77bd07f9f7bd851f181798
Tudor Turcanu <tudor.turcanu@cgm.com>
2021-03-15 17:22:41 +0200

    - fixed typo

--- AsyncAwait/AsyncAwait/URLSession+Extensions.swift
+++ AsyncAwait/AsyncAwait/URLSession+Extensions.swift
@@ -5,22 +5,21 @@
 //  Created by Tudor Turcanu on 15.03.2021.
 //
 
-import Foundation
 import _Concurrency
+import Foundation
 
 extension URLSession {
     func dataTask(url: URL) async -> (Data?, URLResponse?, Error?) {
-        return await withUnsafeContinuation({ continuation in
+        return await withUnsafeContinuation { continuation in
             dataTask(with: url) { data, response, error in
-                continuation.resume(returning:  (data, response, error))
+                continuation.resume(returning: (data, response, error))
             }.resume()
-        })
+        }
     }
 }
 
 class NetworkRequest {
     @asyncHandler func getData() {
-        
         let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
         let (data, response, error) = await URLSession.shared.dataTask(url: url)
         print(String(data: data!, encoding: .utf8))
